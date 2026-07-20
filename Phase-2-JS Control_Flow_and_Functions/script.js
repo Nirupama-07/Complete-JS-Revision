3e8791f48724ca55d55dd50fca8f1fa13829b2c8
Nirupama <nirupamamajhi549@gmail.com>
2026-07-20 22:51:25 +0530

    Updated some changes

--- Phase-2-JS Control_Flow_and_Functions/script.js
+++ Phase-2-JS Control_Flow_and_Functions/script.js
@@ -14,11 +14,24 @@ if(age>18){
 }*/
 
 //if..else-if..else statement
-let age=parseInt(prompt("Enter your age"))
+/*let age=parseInt(prompt("Enter your age"))
 if(age>18){
     console.log("You can drive")
 }else if(age<0){
     console.log("Age cannot be negative")
 }else{
     console.log("You cannot drive")
+}*/
+
+//nested if
+let age=parseInt(prompt("Enter your age"))
+if(age>20 && age<29){
+    console.log("You are in your 20's")
+    if(age%2==0){
+        console.log("Your age is even")
+    }else{
+        console.log("Your age is odd")
+    }
+}else{
+    console.log("Your age does not lies between 20 and 29")
 }
