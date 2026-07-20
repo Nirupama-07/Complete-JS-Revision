122744fd025d15918e35ffbe855885305caebf02
Nirupama <nirupamamajhi549@gmail.com>
2026-07-20 21:45:47 +0530

    Pending chapter-2

---
+++ Phase-2-JS Control_Flow_and_Functions/script.js
@@ -0,0 +1,24 @@
+// if statement
+/*let age=parseInt(prompt("Enter your age"))
+
+if(age>18){
+    console.log("You can drive")
+}*/
+
+//if-else statement
+/*let age=parseInt(prompt("Enter your age"))
+if(age>18){
+    console.log("You can drive")
+}else{
+    console.log("You cannot drive")
+}*/
+
+//if..else-if..else statement
+let age=parseInt(prompt("Enter your age"))
+if(age>18){
+    console.log("You can drive")
+}else if(age<0){
+    console.log("Age cannot be negative")
+}else{
+    console.log("You cannot drive")
+}
