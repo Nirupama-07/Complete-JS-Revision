363fe2cec4e3e471513600fe7ab13909bd576929
Nirupama <nirupamamajhi549@gmail.com>
2026-07-21 10:06:03 +0530

    Updated few more

--- Phase-2-JS Control_Flow_and_Functions/script.js
+++ Phase-2-JS Control_Flow_and_Functions/script.js
@@ -87,8 +87,71 @@ greet()*/
 //This is also an example of named function or function declaration
 
 //hoisted defination function
-greet()
+/*greet()
 
 function greet() {
     console.log("Hi")
+}*/
+
+//arrow function
+/*const square=n=>n**n
+console.log(square(2))*/
+
+//rest parameter
+/*function sum(...numbers){
+    let total=0
+    for(let num of numbers){
+         total+=num
+    }
+    return total
 }
+console.log(sum(10,20,30))*/
+
+//callback function
+/*function greet(name,callback){
+    console.log(`Hello ${name}`)
+    callback()
+}
+function say(){
+    console.log("Good morning")
+}
+greet("Rahul",say)
+//here hi is the callback function*/
+
+//filter
+/*const n=[1,2,3,4,5]
+
+const even=n.filter((num)=>num%2==0)
+console.log(even)*/
+
+//reduce
+/*const n=[1,2,3,4,5]
+
+const sum=n.reduce((acc,curr)=>acc+curr,0)
+console.log(sum)*/
+//acc->store the result of the value sum
+//0->initial value
+
+
+//some() and every()
+/*const n = [1, 2, 3, -4, 5]
+const hasNeg = n.some((num) => num > 0)
+console.log(hasNeg)
+
+const n2 = [1, 2, 3, -4, 5]
+const hasNeg2 = n2.every((num) => num > 0)
+console.log(hasNeg2)*/
+
+//function binding
+const student={
+    name:"Nirupama",
+    age:21
+}
+function sayHi(area){
+    console.log(this.name,'is',this.age,'years old',area)
+}
+
+sayHi.call(student,'Bhubaneswar')
+sayHi.apply(student,['Bhubaneswar'])
+let result=sayHi.bind(student,'Bhubaneswar')
+result()
