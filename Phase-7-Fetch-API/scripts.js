178f3f926082e455349de6126705025d34a44057
Nirupama <nirupamamajhi549@gmail.com>
2026-08-08 08:49:30 +0530

    fetch api completed localstorage pending

--- Phase-7-Fetch-API/scripts.js
+++ Phase-7-Fetch-API/scripts.js
@@ -7,7 +7,9 @@
         console.log(error);
     });*/
 
-async function getUsers() {
+
+//get
+/*async function getUsers() {
     try{
         const response=await fetch("https://jsonplaceholder.typicode.com/users")
 
@@ -22,4 +24,85 @@ async function getUsers() {
         
     }
 }
-getUsers()
+getUsers()*/
+
+//post
+/*async function createUsers() {
+    const user={
+        name:"Nirupama",
+        email:"nirupama@gmail.com"
+    }
+
+    try{
+        const response=await fetch("https://jsonplaceholder.typicode.com/users",
+        {
+            method: "POST",
+
+            headers: {
+                "Content-Type": "application/json"
+            },
+
+            body: JSON.stringify(user)
+        }
+        )
+        const data=await response.json()
+        console.log(data);
+        
+    }catch(error){
+        console.log(error);
+    }
+}
+createUsers()*/
+
+/*async function deleteUser(id) {
+
+    try {
+
+        const response = await fetch(
+            `https://jsonplaceholder.typicode.com/users/${id}`,
+            {
+                method: "DELETE"
+            }
+        );
+
+        if (!response.ok) {
+            throw new Error("Failed to delete user");
+        }
+
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+deleteUser(1);*/
+
+
+let btn=document.getElementById("btn")
+let usersDiv=document.getElementById("users")
+btn.addEventListener('click',async()=>{
+    try{
+        const response = await fetch(
+            "https://jsonplaceholder.typicode.com/users"
+        );
+
+        if (!response.ok) {
+            throw new Error("Something went wrong");
+        }
+
+        const users = await response.json();
+
+        usersDiv.innerHTML = "";
+
+        users.forEach(user => {
+
+            const p = document.createElement("p");
+
+            p.textContent = user.name;
+
+            usersDiv.appendChild(p);
+        });
+
+    }catch(error){
+        console.log(error);
+    }
+})
